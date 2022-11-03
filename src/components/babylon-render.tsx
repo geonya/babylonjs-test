6f28da16b553542f7756710209165f77184bd219
geonya <geony8410@gmail.com>
2022-11-03 17:24:57 +0900

    box color change

--- src/components/babylon-render.tsx
+++ src/components/babylon-render.tsx
@@ -32,22 +32,34 @@ const onSceneReady = (scene: BABYLON.Scene) => {
   )
   light.intensity = 0.8
 
-  const box = BABYLON.MeshBuilder.CreateBox('box', { size: 2 }, scene)
-
+  const box = BABYLON.MeshBuilder.CreateBox('box', {})
+  box.position.x = 0.5
   box.position.y = 1
+
+  const boxMaterial = new BABYLON.StandardMaterial('material', scene)
+  boxMaterial.diffuseColor = BABYLON.Color3.Random()
+  box.material = boxMaterial
+
+  box.actionManager = new BABYLON.ActionManager(scene)
+  box.actionManager.registerAction(
+    new BABYLON.ExecuteCodeAction(
+      BABYLON.ActionManager.OnPickTrigger,
+      function (evt) {
+        const sourceBox = evt.meshUnderPointer
+        //move the box upright
+        sourceBox!.position.x += 0.1
+        sourceBox!.position.y += 0.1
+        //update the color
+        boxMaterial.diffuseColor = BABYLON.Color3.Random()
+      },
+    ),
+  )
 }
 
 /**
  * Will run on every frame render.  We are spinning the box on y-axis.
  */
-const onRender = (scene: BABYLON.Scene) => {
-  if (box !== undefined) {
-    var deltaTimeInMillis = scene.getEngine().getDeltaTime()
-
-    const rpm = 10
-    box.rotation.y += (rpm / 60) * Math.PI * 2 * (deltaTimeInMillis / 500)
-  }
-}
+const onRender = (scene: BABYLON.Scene) => {}
 
 export default () => (
   <div>
